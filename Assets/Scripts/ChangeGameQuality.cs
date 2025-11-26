f8d0a2e4a7afb3062e767cfdda64ed2219e63839
ShinyMattC <christians.mathew@gmail.com>
2025-11-26 16:33:39 +0200

    Added a graphics setting modifier, from Very Low to Ultra. Added that to the settings UI, which was also now made as well, but is still basic

---
+++ Assets/Scripts/ChangeGameQuality.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ChangeGameQuality : MonoBehaviour
+{
+    public TMP_Dropdown dropdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropdown.value = PlayerPrefs.GetInt("graphicsTier");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeGraphics(int index) {
+        QualitySettings.SetQualityLevel(index);
+        Debug.Log(QualitySettings.GetQualityLevel());
+        PlayerPrefs.SetInt("graphicsTier", QualitySettings.GetQualityLevel());
+    }
+}
--- Assets/Scripts/SceneButton.cs
+++ Assets/Scripts/SceneButton.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 public class SceneButton : MonoBehaviour
 {
     public void ButtonSceneLoad(int sceneIndex)
     {
+        QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("graphicsTier"));
         SceneManager.LoadSceneAsync(sceneIndex);
     }
 
